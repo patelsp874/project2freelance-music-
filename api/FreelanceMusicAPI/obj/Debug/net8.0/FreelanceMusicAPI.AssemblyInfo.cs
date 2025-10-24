03d0d3d6e29f37ead72aa5e4617725d1f00cf7b0
srujal patel <slpatel@crimson.ua.edu>
2025-10-24 02:42:44 -0500

    starting admin

--- api/FreelanceMusicAPI/obj/Debug/net8.0/FreelanceMusicAPI.AssemblyInfo.cs
+++ api/FreelanceMusicAPI/obj/Debug/net8.0/FreelanceMusicAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreelanceMusicAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baad1ef55b99d9efd9f1040f051286bf67342e36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0f92b1930acbccf232d88a33d1e2a019c543c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreelanceMusicAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreelanceMusicAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
